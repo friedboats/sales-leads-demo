756265cff9f412e89022a3180ec437d2682c8a5a
Nick Bence <nickbence@helloworld.com>
2019-10-02 23:55:39 -0400

    add groundhog and font awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { TopBarComponent } from './top-bar/top-bar.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
