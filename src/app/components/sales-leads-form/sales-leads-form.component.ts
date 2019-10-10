e344564062737757d89f5df7371af4e8720459ab
Nick Bence <nickbence@helloworld.com>
2019-10-09 20:45:34 -0400

    add form validator checks for value and ISO

--- src/app/components/sales-leads-form/sales-leads-form.component.ts
+++ src/app/components/sales-leads-form/sales-leads-form.component.ts
@@ -15,8 +15,6 @@ export class SalesLeadsFormComponent implements OnInit {
   submitAttempted = false;
   submitInProgress = false;
 
-  // TODO Put in special errors for leadAmount and leadDate
-
   constructor(private formBuilder: FormBuilder, private salesLeadService:SalesLeadService) { }
 
   ngOnInit() {
@@ -25,7 +23,7 @@ export class SalesLeadsFormComponent implements OnInit {
       salesRep: ['', Validators.required],
       clientName: ['', Validators.required],
       leadAmount: ['', Validators.required],
-      leadDate: ['', Validators.required]
+      leadDate: ['', Validators.pattern('^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(.[0-9]+)?(Z)?$')]
     });
 
     // Subscribe to when the form makes a successful post so we can update our compontent with the new data
